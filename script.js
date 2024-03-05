d69607df51fe09984de79e63810ecab8c4b89f7e
jmartincampos <jmartincampos18@gmail.com>
2024-03-04 18:47:16 -0800

    script tentative

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add your JavaScript here
+document.getElementById('toggleBio').addEventListener('click', function() {
+    const bio = document.getElementById('about');
+    bio.classList.toggle('bio-animation');
+  });
+  
